25c26d95a5e6a0fa5c441ac6e702f37fca739d03
Phat <phatdroid94@gmail.com>
2023-06-02 21:02:21 +0700

    ui

---
+++ app/src/main/java/com/example/weatherapp20022023/data/model/Demo.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp20022023.data.model
+
+/**
+ * Created by pphat on 5/31/2023.
+ */
+class Demo {
+
+
+}
--- app/src/main/java/com/example/weatherapp20022023/data/remote/ApiService.kt
+++ app/src/main/java/com/example/weatherapp20022023/data/remote/ApiService.kt
@@ -1,7 +1,18 @@
 package com.example.weatherapp20022023.data.remote
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * Created by pphat on 5/31/2023.
  */
 interface ApiService {
+
+    //data/2.5/weather?appid=2206d84c8189efe365465e3318f487aa&units=metric&q=Ho Chi Minh
+    @GET("data/2.5/weather")
+    suspend fun callTempFromCityName(
+        @Query("appid") appid: String,
+        @Query("units") units: String,
+        @Query("q") q: String,
+    )
 }
